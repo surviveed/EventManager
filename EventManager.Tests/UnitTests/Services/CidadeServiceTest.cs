a958c698ae9c85d68a3ec393d9f1a161af77ace4
Lucas Davi <lucas@fullsoft.com.br>
2024-09-16 10:47:58 -0300

    test: added crud test for cidades

--- EventManager.Tests/UnitTests/Services/CidadeServiceTest.cs
+++ EventManager.Tests/UnitTests/Services/CidadeServiceTest.cs
@@ -37,6 +37,67 @@ namespace EventManager.Tests.UnitTests.Services
             Assert.AreEqual(inMemoryCidade.UfId, cidade.UfId);
         }
 
+        [TestMethod]
+        public void ShouldBeFindAllCidade()
+        {
+
+            inMemoryCidadeRepository.items.Add(new MakeCidade().GetData(id: 1, descricao: "cidade 1"));
+            inMemoryCidadeRepository.items.Add(new MakeCidade().GetData(id: 2, descricao: "cidade 2"));
+            inMemoryCidadeRepository.items.Add(new MakeCidade().GetData(id: 3, descricao: "cidade 3"));
+
+            var result = sut.BuscarTodos();
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Count == 3);
+            Assert.IsTrue(result.Any(x => x.Id == 1 && x.Descricao == "cidade 1"));
+            Assert.IsTrue(result.Any(x => x.Id == 2 && x.Descricao == "cidade 2"));
+            Assert.IsTrue(result.Any(x => x.Id == 3 && x.Descricao == "cidade 3"));
+
+        }
+
+        [TestMethod]
+        public void ShouldBeFindByIdCidade()
+        {
+
+            inMemoryCidadeRepository.items.Add(new MakeCidade().GetData(id: 1, descricao: "cidade 1"));
+            inMemoryCidadeRepository.items.Add(new MakeCidade().GetData(id: 2, descricao: "cidade 2"));
+            inMemoryCidadeRepository.items.Add(new MakeCidade().GetData(id: 3, descricao: "cidade 3"));
+
+            var result = sut.BuscarPorId(2);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Id == 2 && result.Descricao == "cidade 2");
+
+        }
+
+        [TestMethod]
+        public void ShouldBeDeleteCidade()
+        {
+
+            inMemoryCidadeRepository.items.Add(new MakeCidade().GetData(id: 1, descricao: "cidade 1"));
+            inMemoryCidadeRepository.items.Add(new MakeCidade().GetData(id: 2, descricao: "cidade 2"));
+            inMemoryCidadeRepository.items.Add(new MakeCidade().GetData(id: 3, descricao: "cidade 3"));
+
+            sut.Remover(2);
+
+            Assert.IsNotNull(inMemoryCidadeRepository.items);
+            Assert.IsTrue(inMemoryCidadeRepository.items.Count == 2);
+            Assert.IsFalse(inMemoryCidadeRepository.items.Any(x => x.Id == 2));
+        }
+
+        [TestMethod]
+        public void ShouldBeUpdateCidade()
+        {
+
+            inMemoryCidadeRepository.items.Add(new MakeCidade().GetData(id: 1, descricao: "cidade 1"));
+            inMemoryCidadeRepository.items.Add(new MakeCidade().GetData(id: 2, descricao: "cidade 2"));
+            inMemoryCidadeRepository.items.Add(new MakeCidade().GetData(id: 3, descricao: "cidade 3"));
+
+            sut.Atualizar(new MakeCidade().GetDataDto(id: 2, descricao: "Updated cidade 2"));
+
+            Assert.IsTrue(inMemoryCidadeRepository.items.Any(x => x.Id == 2 && x.Descricao == "Updated cidade 2"));
+        }
+
     }
 
 }
